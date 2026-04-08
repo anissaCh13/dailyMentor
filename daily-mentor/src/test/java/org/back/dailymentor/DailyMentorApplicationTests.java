e798f23209186a0ae84dfe18a8acade49f47e2fc
Anissa <anissa.ch.cours@gmail.com>
2026-04-08 21:42:05 +0200

    feat(001): Project initialization and addition of the Discord bot, tests passing successfully.

---
+++ daily-mentor/src/test/java/org/back/dailymentor/DailyMentorApplicationTests.java
@@ -0,0 +1,11 @@
+package org.back.dailymentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyMentorApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
