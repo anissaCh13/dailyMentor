f2e794752567d04028d59c663d6662944b2edbf3
Anissa <anissa.ch.cours@gmail.com>
2026-04-10 09:19:32 +0200

    feat(bot-002): Create DiscordService to enable automatic bot messages and update DiscordListener to handle yes/no responses, with tests implemented via DiscordStartupTest.

---
+++ daily-mentor/src/main/java/org/back/dailymentor/discord/DiscordService.java
@@ -0,0 +1,26 @@
+package org.back.dailymentor.discord;
+
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DiscordService {
+
+  private final DiscordBot discordBot;
+
+
+  public void sendPrivateMessage(String userId, String message) {
+
+    JDA jda = discordBot.getJda();
+
+    User user = jda.retrieveUserById(userId).complete();
+
+    user.openPrivateChannel()
+        .flatMap(channel -> channel.sendMessage(message))
+        .queue();
+  }
+
+}
