15f3ad434cbdd8c2bb459a82d40d5981ff194ac6
Anissa <anissa.ch.cours@gmail.com>
2026-04-13 09:44:23 +0200

    feat(bot-005): Implement scheduler to run job daily at 8 AM

--- daily-mentor/src/main/java/org/back/dailymentor/DailyMentorApplication.java
+++ daily-mentor/src/main/java/org/back/dailymentor/DailyMentorApplication.java
@@ -2,8 +2,10 @@ package org.back.dailymentor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DailyMentorApplication {
 
   public static void main(String[] args) {
